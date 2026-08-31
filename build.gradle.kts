f136a8f0c8f95d711805307266dd71b6b061b1e8
korhan <korhandoganaykarsln@gmail.com>
2026-08-31 23:10:33 +0300

    fix: pin gradle plugin to verified JitPack version master-69fdb8fc4b-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.2")
-        classpath("com.github.recloudstream:gradle:master")
+        classpath("com.github.recloudstream:gradle:master-69fdb8fc4b-1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
